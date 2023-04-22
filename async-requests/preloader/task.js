aa9a70a88acbc8fe1fb6e9e2ed732603f9f72333
Semen2022 <study2022@mail.ru>
2023-04-22 23:34:36 +0300

    Задача 3.1 решена

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -7,14 +7,20 @@ xhrRate.send();
 
 xhrRate.addEventListener('readystatechange', () => {
     if (xhrRate.readyState === xhrRate.DONE) {
-        console.log(xhrRate.response);
+        // console.log(xhrRate.response);
         document.getElementById("loader").classList.remove("loader_active");
 
         const ObjectRate = JSON.parse(xhrRate.response)['response']['Valute'];
-        console.log('***---***---***' + ObjectRate + '***////***////');
-
+        // console.log('***---***---***' + ObjectRate + '***////***////');
+        const insertElement = document.getElementById('items');
+        
         for (const item in ObjectRate) {
-            console.log('PrinT ', ObjectRate[item].Value , ObjectRate[item].CharCode);
+            // console.log('PrinT ', ObjectRate[item].CharCode, ObjectRate[item].Value, ' руб.' );
+            let output = '<div class="item">';
+            output +='<div class="item__code">';
+            output += ObjectRate[item].CharCode + '</div><div class="item__value">' + ObjectRate[item].Value;  
+            output += '</div><div class="item__currency">руб.</div></div>';
+            insertElement.insertAdjacentHTML("afterend", output);            
         }
     } 
 })
