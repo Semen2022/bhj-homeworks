9d9f65a573d31555a0b2e0b1f2814a220213858b
Semen2022 <study2022@mail.ru>
2023-04-25 22:20:57 +0300

    Задача Редактор почти решена

---
+++ client-state/text-editor/task.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const storageText = localStorage.getItem('text');
+
+if (storageText) {
+    document.getElementById("editor").textContent = storageText;
+}
+const textField = document.getElementById("editor");
+textField.addEventListener('keydown', () => {
+    localStorage.setItem('text', textField.value);
+})
