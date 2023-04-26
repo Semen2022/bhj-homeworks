ed3b3ef16e6dc1230c7faf9c0287daad9ac12b5a
Semen2022 <study2022@mail.ru>
2023-04-26 23:55:54 +0300

    Немного продвинулся в решении

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,20 +1,27 @@
 'use strict'
 
-const singIn = document.getElementById('singin');
+const signIn = document.getElementById('signin');
 const welcome = document.getElementById('welcome');
 const userId = document.getElementById('user_id');
 
 document.forms.signin__form.addEventListener('submit', (ev) => {
     ev.preventDefault();
-
+console.log('Кнопка нажата');
     const formData = new FormData(document.forms.signin__form);
     const xhrReg = new XMLHttpRequest();
 
     xhrReg.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
+    xhrReg.responseType = 'json';
     xhrReg.addEventListener('readystatechange', () => {
         if (xhrReg.readyState === xhrReg.DONE) {
-            localStorage.setItem(xhrReg.response.user__id);
-            welcome.textContent = user__id;
+            localStorage.setItem('user_id', xhrReg.response.user_id);
+            console.log(xhrReg.response);
+            console.log(xhrReg.response['user_id']);
+
+            console.log('signIn', signIn);
+            signIn.classList.remove("signin_active");
+            welcome.classList.add("welcome_active");
+            userId.textContent = xhrReg.response.user_id;
         }
     })
     
