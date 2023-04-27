e1241a94cfc619e8d0398df2561c5b5e53672b6b
Semen2022 <study2022@mail.ru>
2023-04-27 08:11:29 +0300

    Залдача 3.2 "Авторизация" решена

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -3,32 +3,38 @@
 const signIn = document.getElementById('signin');
 const welcome = document.getElementById('welcome');
 const userId = document.getElementById('user_id');
+const storagedName = localStorage.getItem('user_id');
 
-document.forms.signin__form.addEventListener('submit', (ev) => {
-    ev.preventDefault();
-console.log('Кнопка нажата');
-    const formData = new FormData(document.forms.signin__form);
-    const xhrReg = new XMLHttpRequest();
-
-    xhrReg.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
-    xhrReg.responseType = 'json';
-    xhrReg.addEventListener('readystatechange', () => {
-        if (xhrReg.readyState === xhrReg.DONE) {
-            localStorage.setItem('user_id', xhrReg.response.user_id);
-            console.log(xhrReg.response);
-            console.log(xhrReg.response['user_id']);
-
-            console.log('signIn', signIn);
-            signIn.classList.remove("signin_active");
-            welcome.classList.add("welcome_active");
-            userId.textContent = xhrReg.response.user_id;
-        }
+if (storagedName) {
+    signIn.classList.remove("signin_active");
+    welcome.classList.add("welcome_active");
+    userId.textContent = storagedName;
+} else {
+    document.forms.signin__form.addEventListener('submit', (ev) => {
+        ev.preventDefault();
+        // console.log('Кнопка нажата');
+        const formData = new FormData(document.forms.signin__form);
+        const xhrReg = new XMLHttpRequest();
+    
+        xhrReg.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
+        xhrReg.responseType = 'json';
+        xhrReg.addEventListener('readystatechange', () => {
+            if (xhrReg.readyState === xhrReg.DONE) {
+                if (xhrReg.response.success) {
+                    localStorage.setItem('user_id', xhrReg.response.user_id);
+                    signIn.classList.remove("signin_active");
+                    welcome.classList.add("welcome_active");
+                    userId.textContent = xhrReg.response.user_id;
+                } else {
+                    //signIn.classList.remove("signin_active");
+                    alert("Неверный логин/пароль");
+                }
+                
+            }
+        })
+        xhrReg.send(formData);
     })
-    
-    
-    xhrReg.send(formData);
+}
 
 
-
-})
 
